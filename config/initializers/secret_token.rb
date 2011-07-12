dd30ef695bdc8e1f08ccb58eef35cbbc476044de
Vincent Tencé <vtence@gmail.com>
2011-07-11 23:09:58 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coucou::Application.config.secret_token = '21b8cd5efa7e00b91857a8b12802dcaa3e5dabe4a4dfd4883a89aff97687f096e0c013344ad6cbd7aa53ca99892c3a1d412510aad3c8ab654baa1e161e8d6c66'
---
+++ features/get_advice.feature
@@ -0,0 +1,12 @@
+Feature: Involve a circle
+  In order to make the best decision
+  As a proposer
+  I want to leverage the wisdom of my circle
+
+
+  Scenario: drop it when there is no vote
+    Given a proposition
+    When there is no vote
+    Then the advice is to drop the proposition
+
+
